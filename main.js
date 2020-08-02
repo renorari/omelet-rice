818c5cf547106906ec5406e3b022d9cc018619c5
Glitch (hello-express) <none>
2020-08-02 10:21:38 +0000

    🌅🖼 Checkpoint ./main.js:13668630/12781 ./views/index.html:13668630/572

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ client.on("message", message => {
   if (m == "om!help") {
     message.channel.send("準備中...");
   } else if (m == "om!ping") {
-    message.channel.send(`現在のBotのPing値は、${client.ws.ping}msです。`); //ここだなぜ？
+    message.channel.send(`現在のBotのPing値は、${client.ws.ping}msです。`);
   }
 });
 
@@ -29,12 +29,12 @@ client.login(process.env.DISCORD_BOT_TOKEN + "tintin"); //エラー吐かない.
 
 function getType(_url) {
   var types = {
-    ".html": "text/html",
-    ".css": "text/css",
-    ".js": "text/javascript",
+    ".html": "text/html; charset=utf-8",
+    ".css": "text/css; charset=utf-8",
+    ".js": "text/javascript; charset=utf-8",
     ".png": "image/png",
     ".gif": "image/gif",
-    ".svg": "svg+xml"
+    ".svg": "svg+xml; charset=utf-8"
   };
   for (var key in types) {
     if (_url.endsWith(key)) {
@@ -52,8 +52,16 @@ var server = http.createServer(function(req, res) {
   if (fs.existsSync(url)) {
     fs.readFile(url, (err, data) => {
       if (!err) {
-        res.writeHead(200, { "Content-Type": getType(url) });
-        res.end(data);
+        if (url.endsWith(".html")) {
+          res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+          res.end(
+            data +
+              '\n<center><script type="text/javascript" src="//rot6.a8.net/jsa/822e75fcc79c79da0bcd4a5c6b0af6a0/93dd4de5cddba2c733c65f233097f05a.js"></script></center>'
+          );
+        } else {
+          res.writeHead(200, { "Content-Type": getType(url) });
+          res.end(data);
+        }
       } else {
         res.statusCode = 500;
         res.end();
