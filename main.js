7cd21b08fbc27f7bdcb98e81d2ffec7993a0e6f2
Glitch (hello-express) <none>
2020-08-02 11:07:11 +0000

    🚝🎢 Checkpoint ./main.js:13668630/1720 ./views/ads/01.txt:13668630/157 ./views/ads/02.txt:13668630/122 ./views/ads/03.txt:13668630/132 ./views/ads/04.txt:13668630/501

--- main.js
+++ main.js
@@ -17,12 +17,60 @@ client.on("ready", () => {
 client.on("message", message => {
   var m = message.content;
   if (m == "om!help") {
-    message.channel.send("準備中...");
+    message.channel.send("申し訳ございません。\n現在、準備中です。");
   } else if (m == "om!ping") {
     message.channel.send(`現在のBotのPing値は、${client.ws.ping}msです。`);
   }
 });
 
+//広告
+var aco = 0
+var array = [fs.readFileSync("views/ads/01.txt", "utf8"), fs.readFileSync("views/ads/02.txt", "utf8"), fs.readFileSync("views/ads/03.txt", "utf8"), fs.readFileSync("views/ads/04.txt", "utf8")];
+client.on("message", message => {
+  if (message.content.startsWith("om!")) {
+    aco += 1
+    if (aco >= 3) {
+      aco = 0
+      message.channel.send({
+        "content": "オム丼 - 広告",
+        "embed": {
+          "title": "オム丼 - 広告",
+          "description": "[この広告について](http://www.onamae.com/)",
+          "color": 0x97f6e1,
+          "timestamp": new Date(),
+          "footer": {
+            "text": "ＧＭＯインターネット株式会社「東証一部上場企業」"
+          },
+          "fields": [
+            {
+              "name": "独自ドメインの取得なら１円～の【お名前.com】",
+              "value": array[Math.floor(Math.random() * array.length)]
+            }
+          ]
+        }
+      })
+    }
+  } else if (message.content == "om!広告") {
+    message.channel.send({
+      "content": "オム丼 - 広告",
+      "embed": {
+        "title": "オム丼 - 広告",
+        "description": "[この広告について](http://www.onamae.com/)",
+        "color": 0x97f6e1,
+        "timestamp": new Date(),
+        "footer": {
+          "text": "ＧＭＯインターネット株式会社「東証一部上場企業」"
+        },
+        "fields": [
+          {
+            "name": "独自ドメインの取得なら１円～の【お名前.com】",
+            "value": array[Math.floor(Math.random() * array.length)]
+          }
+        ]
+      }
+    })
+  }
+})
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 //--------------------//
